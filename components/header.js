e7259f1403d13b7ba7e1429e522aee6e1968cc65
Niferu <wargacki.patryq@gmail.com>
2023-09-19 23:42:58 +0200

    formatting update

--- components/header.js
+++ components/header.js
@@ -8,18 +8,22 @@ import { links } from "@/lib/data"
 export default function Header() {
     return (
         <header className={header.header}>
-            <motion.div className={header.nav} initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
+            <motion.div className={header.motion} initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
                 <Link className={header.title} href="/">
                     Niferu <span className={header.dot}>.</span>
                 </Link>
-                {links.map((link) => (
-                    <motion.li key={link.hash} className={header.li}>
-                        <Link className={header.underline} href={link.hash}>
-                            {link.name}{" "}
-                        </Link>
-                    </motion.li>
-                ))}
             </motion.div>
+            <nav className={header.nav}>
+                <ul className={header.ul}>
+                    {links.map((link) => (
+                        <motion.li key={link.hash} className={header.li}>
+                            <Link className={header.underline} href={link.hash}>
+                                {link.name}{" "}
+                            </Link>
+                        </motion.li>
+                    ))}
+                </ul>
+            </nav>
         </header>
     )
 }
