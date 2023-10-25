39d43aea49c13aaaadf1c8bd518a05bd16948bf1
Niferu <wargacki.patryq@gmail.com>
2023-10-25 20:06:32 +0200

    submit btn fixed

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -7,15 +7,15 @@ export default function SubmitButton() {
 
     return (
         <button
-            className="group flex items-center self-center justify-center w-[7rem] gap-2 py-[0.7rem] sm:py-[0.9rem] text-alfa dark:hover:text-god borderBlack borderDevil bg-black bg-opacity-50 rounded-full focus:scale-110 hover:scale-110 hover:bg-black hover:bg-opacity-80 active:scale-105 duration-custom ease-customBezier hover:translate-y-[-4px] disabled:scale-100 disabled:bg-opacity-50"
+            className="group flex font-semibold items-center self-center justify-center w-[7rem] gap-2 py-[0.7rem] sm:py-[0.9rem] text-nite dark:text-alfa hover:text-lord dark:hover:text-god borderBlack borderDevil bg-white bg-opacity-50 dark:bg-black dark:bg-opacity-50 rounded-full focus:scale-110 hover:scale-110 hover:bg-white hover:bg-opacity-100 dark:hover:bg-black dark:hover:bg-opacity-80 active:scale-105 duration-custom ease-customBezier hover:translate-y-[-4px] disabled:scale-100 disabled:bg-opacity-50"
             type="submit"
             disabled={pending}
         >
             {pending ? (
-                <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-god"></div>
+                <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-lord dark:border-god"></div>
             ) : (
                 <>
-                    Submit <FaPaperPlane className="text-sm opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
+                    Submit <FaPaperPlane className="text-sm opacity-80 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
                 </>
             )}
         </button>
