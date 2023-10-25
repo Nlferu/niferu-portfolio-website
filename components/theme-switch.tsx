00834a755d6779e3985b50da86d299aa84191f1c
Niferu <wargacki.patryq@gmail.com>
2023-10-25 20:32:46 +0200

    btn switch to be reworked

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -11,7 +11,7 @@ export default function ThemeSwitch() {
 
     return (
         <button
-            className="flex fixed items-center border-solid border-[0.2px] border-nighty hover:border-nightGod dark:border-alfa shadow-night dark:shadow-sun hover:shadow-nightA dark:hover:shadow-sunA rounded-full justify-center backdrop-blur-[0.5rem] bottom-12 right-12 text-nighty hover:text-nightGod dark:text-alfa dark:hover:text-god h-10 w-10 text-2xl hover:scale-[1.15] active:scale-105 transition-all"
+            className="flex fixed items-center bg-stone-500 bg-opacity-100 dark:bg-opacity-0 border-solid border-[0.2px] border-nite hover:border-lord dark:border-alfa shadow-night dark:shadow-sun hover:shadow-nightA dark:hover:shadow-sunA rounded-full justify-center backdrop-blur-[0.5rem] bottom-12 right-12 text-nite hover:text-lord dark:text-alfa dark:hover:text-god h-10 w-10 text-2xl hover:scale-[1.15] active:scale-105 transition-all"
             onClick={toggleTheme}
         >
             {theme === "light" ? <BsMoon /> : <BsSun />}
