b78f98d2c96713c3a0a1621538ed2e0b73fe2585
Niferu <wargacki.patryq@gmail.com>
2024-01-28 21:00:42 +0100

    about desc update

--- components/about.tsx
+++ components/about.tsx
@@ -18,11 +18,16 @@ export default function About() {
             id="about"
         >
             <SectionHeading>About Me</SectionHeading>
-            <p className="text-stone-950 dark:text-gray-400 font-medium mb-5 text-lg">
-                I&apos;ve spent past 7 years mastering finance industry by working for some of the top American banks in investment sector and trading various
-                markets on my own. Since mid of 2020 I have started my programming journey from taking beginner Java course, after which I moved into
-                programming trading bots and indicators for forex market. My next step was delving into the exciting world of blockchain technology, exploring
-                its endless possibilities since early 2022 until now. I am highly motivated, self-driven, and detail-oriented.
+            <p className="text-stone-950 dark:text-gray-400 font-medium mb-5 text-lg flex flex-col">
+                I&apos;ve spent the past eight years mastering the finance industry. My interests in trading across diverse markets like stocks, crypto,
+                futures, and more pushed me into learning programming so I could test and automate my trading ideas. The ability to check all my strategies and
+                ideas very quickly using code made me love programming.
+                <span className="my-4">
+                    I have finally come to realize that blockchain is this great mix of finance, programming, and decentralization a perfect fit for my skills
+                    and passions. That made me start learning more languages and frameworks that allowed me to create applications within web 3.0 from start to
+                    end. I&apos;m currently applying my skills by fully committing to these new technologies and transitioning to a full-time role in full-stack
+                    development.
+                </span>
             </p>
             <p className="text-stone-950 dark:text-gray-400 font-medium text-lg">
                 When I&apos;m not programming I like playing blitz and bullet chess, video games or do skateboarding!
