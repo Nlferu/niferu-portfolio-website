e3d32d67da448ac1f8279567bb20e54743096b85
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-01-22 17:13:36 +0100

    description update

--- components/about.tsx
+++ components/about.tsx
@@ -19,13 +19,15 @@ export default function About() {
         >
             <SectionHeading>About Me</SectionHeading>
             <p className="text-stone-950 dark:text-gray-400 font-medium mb-5 text-lg flex flex-col">
-                I&apos;ve spent the past eight years mastering the finance industry. My interests in trading across diverse markets like stocks, crypto,
-                futures, and more pushed me into learning programming so I could test and automate my trading ideas. The ability to check all my strategies and
-                ideas very quickly using code made me love programming.
+                With over eight years of experience in the finance industry, including working for top American banks in the investment sector and trading
+                various markets independently, I developed a strong interest in trading diverse assets like stocks, crypto, and futures. This passion pushed me
+                to learn programming so I could test and automate my trading ideas. The ability to quickly validate my strategies using code made me fall in
+                love with programming.
                 <span className="my-4">
                     I have finally come to realize that blockchain is this great mix of finance, programming, and decentralization a perfect fit for my skills
                     and passions. That made me start learning more languages and frameworks that allowed me to create applications within web 3.0 from start to
-                    end. I&apos;m currently applying my skills by fully committing to these new technologies working as a blockchain engineer.
+                    end. I&apos;m currently applying my skills by fully committing to these new technologies working as a{" "}
+                    <span className="text-darklord dark:text-orange-400 underline decoration-2 underline-offset-2">blockchain and backend engineer</span>.
                 </span>
             </p>
             <p className="text-stone-950 dark:text-gray-400 font-medium text-lg">
